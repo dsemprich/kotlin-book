9f6d703691b32d284d9359cf7cc40f30211b0191
David Semprich <david.semprich@affinitas.de>
2017-07-12 11:57:31 +0200

    Chapter 6 variables and properties

--- app/src/main/java/de/affinitas/test/kotlinbook/TestClass.kt
+++ app/src/main/java/de/affinitas/test/kotlinbook/TestClass.kt
@@ -1,4 +1,11 @@
 package de.affinitas.test.kotlinbook
 
-import android.widget.Toast
+class Person {
+    // uses default getter and setter
+    //var name : String = ""
 
+    //custom
+    var name = ""
+        get() = field.toUpperCase()
+        set(value) { field = "Name: $value" }
+}
