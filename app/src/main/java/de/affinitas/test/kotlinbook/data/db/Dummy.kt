73f1f745b64ba4eb938ceda8895c5cb02153310b
David Semprich <david.semprich@affinitas.de>
2017-07-13 11:36:38 +0200

    chapter 17 create sqlitehelper

--- app/src/main/java/de/affinitas/test/kotlinbook/data/db/Dummy.kt
+++
@@ -1,5 +0,0 @@
-package de.affinitas.test.kotlinbook.data.db
-
-/**
- * Created by dsemprich on 13.07.17.
- */
--- app/src/main/java/de/affinitas/test/kotlinbook/ui/App.kt
+++ app/src/main/java/de/affinitas/test/kotlinbook/ui/App.kt
@@ -1,7 +1,6 @@
 package de.affinitas.test.kotlinbook.ui
 
 import android.app.Application
-import android.database.sqlite.SQLiteOpenHelper
 import de.affinitas.test.kotlinbook.extensions.DelegatesExt
 
 class App : Application() {
@@ -10,13 +9,8 @@ class App : Application() {
         var instance: App by DelegatesExt.notNullSingleValue()
     }
 
-    val database: SQLiteOpenHelper by lazy {
-        MyDatabaseHelper(applicationContext)
-    }
-
     override fun onCreate() {
         super.onCreate()
         instance = this
-        val db = database.writableDatabase
     }
 }
